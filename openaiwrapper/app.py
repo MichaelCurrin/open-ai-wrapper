1de7cfed0430f67eebbdcbc11d11c051e55039ef
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2023-02-21 22:03:42 +0100

    update app.py

--- openaiwrapper/app.py
+++ openaiwrapper/app.py
@@ -2,7 +2,7 @@
 App module.
 """
 import openai
-from flask import Flask, request
+from flask import Flask, jsonify, request
 from flask_restful import Api
 
 app = Flask(__name__)
@@ -19,10 +19,13 @@ def completions():
     Text completions endpoint.
     """
     params = request.json
-    assert params["prompt"]
+
+    if not params.get("prompt"):
+        return jsonify({"error": "Missing param 'prompt'"}), 400
+    if not params.get("engine"):
+        return jsonify({"error": "Missing param 'engine'"}), 400
 
     result = openai.Completion.create(**params)
-
     messages = [choice.text.strip() for choice in result.choices]
 
     return {"response": result.choices, "messages": messages}
